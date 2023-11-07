e9c9d6c9bf00b69503d1aef52b422f2cbd9d9bcc
Adam <A.ergashev@internet.ru>
2023-11-07 23:16:22 +0300

    homework 19.1

---
+++ main.py
@@ -0,0 +1,33 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+
+hostname = 'localhost'
+server_port = 8080
+
+
+class MyServer(BaseHTTPRequestHandler):
+    @staticmethod
+    def __get_html_content():
+        with open('index.html', encoding='utf-8') as index:
+            return index.read()
+
+    def do_GET(self):
+        page = self.__get_html_content()
+
+        self.send_response(200)
+        self.send_header("Content-type", "text/html; charset=utf-8")
+        self.end_headers()
+        self.wfile.write(bytes(page, "utf-8"))
+
+
+if __name__ == '__main__':
+    web_server = HTTPServer((hostname, server_port), MyServer)
+    print('Server started at address http://%s:%s' % (hostname, server_port))
+
+    try:
+        web_server.serve_forever()
+    except KeyboardInterrupt:
+        pass
+
+    web_server.server_close()
+    print('Server stopped')
